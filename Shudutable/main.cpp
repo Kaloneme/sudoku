39b0e8895930e3a047dd29bdc159ced6d55bef9d
Kaloneme <879057619@qq.com>
2017-09-10 23:01:41 +0800

    wrote a readme e

--- Shudutable/main.cpp
+++ Shudutable/main.cpp
@@ -1,15 +1,20 @@
 #include"shudu.h"
 #include<iostream>
 #include<ctime>
-
+#include<cstdio>
 using namespace std;
-int main()
+int main(int argc, char *argv[])
 {
 	//freopen("readin.txt", "r", stdin);
 	freopen("sudoku.txt", "w", stdout);
 	srand(time(NULL));
 	shudu s;
-	cin >> s.n;
+	if (sscanf(argv[2], "%d", &s.n) == 0)
+	{
+		cout << "请输入一个数字：" << endl;
+		return 0;
+	}
+
 	int a = (3 + 0) % 9 + 1;
 	s.table[1][1] =a +'0';
 	s.line[1][a] = 1, s.list[1][a] = 1;
